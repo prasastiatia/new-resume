605eda4b13e499d8454e864106d7cc819a6d82c2
Prasastia Aryani Saliha <tias1508@gmail.com>
2024-11-27 00:34:25 +0700

    Update Resume.jsx

--- utils/Resume.jsx
+++ utils/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
     <Fragment>
       <div className="dark:bg-[#0ab0c2] m-auto mt-6 py-2 px-6 rounded w-fit dark:hover:bg-[#078795] bg-[#f91071] hover:bg-[#c72c6c] text-white font-bold z-10">
         <a
-          href="https://drive.google.com/file/d/1AWiU8oWJi5rY0nl6d3iMHOjevBq7izOU/view?usp=drive_link"
+          href="https://www.canva.com/design/DAGXW6Tr7JE/H027tVSBK7uu4E10mXXkgA/edit?utm_content=DAGXW6Tr7JE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
           target="_blank"
         >
           Resume
